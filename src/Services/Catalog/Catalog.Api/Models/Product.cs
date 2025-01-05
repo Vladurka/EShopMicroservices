3362559a9907678184e27f2410809ef7271506e8
Vladurka <0986535843v@gmail.com>
2025-01-05 17:08:42 +0100

    postProduct

---
+++ src/Services/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Api.Products.CreateProduct;
+
+namespace Catalog.Api.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public List<string> Category { get; set; } = new List<string>();    
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+
+        public Product() { }
+        public Product(CreateProductCommand product)
+        {
+            Name = product.Name;
+            Category = product.Category;
+            Description = product.Description;
+            ImageFile = product.ImageFile;
+            Price = product.Price;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand (string Name, List<string> Category, string Description,
+        string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product(command);
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
