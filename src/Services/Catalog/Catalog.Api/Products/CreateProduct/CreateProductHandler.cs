1102e68a7f72f2212b81b47813a106219c427e1c
Vladurka <0986535843v@gmail.com>
2025-01-11 10:12:32 +0100

    LoggingUpdate

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -15,12 +15,10 @@
         }
     }
 
-    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
-
             var product = new Product(command);
             session.Store(product);
             await session.SaveChangesAsync(cancellationToken);
