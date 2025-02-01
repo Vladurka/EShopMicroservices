a7d85d8501d290d998da9bda278fac0b9d128eae
Vladurka <0986535843v@gmail.com>
2025-02-01 11:31:34 +0100

    AddOrdering

---
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -0,0 +1,14 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+app.Run();
