ff3e60b5660d6ee13ec5d2ac43c5197338a62f06
Vladurka <0986535843v@gmail.com>
2025-02-02 15:06:32 +0100

    DDD Abstractions

---
+++ src/Services/Ordering/Ordering.Core/Abstractions/Aggreagate.cs
@@ -0,0 +1,16 @@
+namespace Ordering.Core.Abstractions;
+
+public abstract class Aggreagate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = new ();    
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    
+    public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return dequeuedEvents;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Core/Abstractions/IAggregate.cs
@@ -0,0 +1,12 @@
+namespace Ordering.Core.Abstractions;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+    
+}
+
+public interface IAggregate : IEntity
+{
+    public IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    public IDomainEvent[] ClearDomainEvents();
+}
