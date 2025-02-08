f1eecac2c64d35057de5ff87de5be76a7ee2714e
Vladurka <0986535843v@gmail.com>
2025-02-08 09:54:43 +0100

    Ordering.Api End

--- src/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,14 +1,25 @@
-﻿namespace Ordering.Api
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+
+namespace Ordering.Api
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApiServices(this IServiceCollection services)
         {
+            services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks();
+            
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
+            app.MapCarter();
+            app.UseExceptionHandler(opts => { });
+            app.UseHealthChecks("/health");
+            
             return app;
         }
     }
---
+++ src/Services/Ordering/Ordering.Api/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Orders.Commands.CreateOrder;
+global using BuildingBlocks.Pagination;
